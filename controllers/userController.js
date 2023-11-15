6946f0792054a1040ce2b5949ed29021453d6dc6
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2023-11-16 01:25:44 +0500

    signup and login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { users } = require("../models/user");
+const { users } = require("../models/userModel");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -82,4 +82,64 @@ const userSignUp = async (req, res) => {
   }
 };
 
-module.exports = { userSignUp };
+const userSignIn = async (req, res) => {
+  try {
+    if (!req.body.emailAddress) {
+      return res.status(400).send({
+        success: false,
+        message: "Email Is Required",
+      });
+    }
+
+    if (!req.body.password) {
+      return res.status(400).send({
+        success: false,
+        message: "Password Is Required",
+      });
+    }
+
+    const checkUser = await users.findOne({
+      emailAddress: req.body.emailAddress,
+    });
+
+    if (!checkUser) {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid Email",
+      });
+    }
+
+    if (
+      checkUser &&
+      (await bcrypt.compare(req.body.password, checkUser.password))
+    ) {
+      const token = jwt.sign(
+        { _id: checkUser._id, emailAddress: checkUser.emailAddress },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "30d",
+        }
+      );
+
+      return res.status(200).send({
+        success: true,
+        message: "User Login Successfully",
+        data: checkUser,
+        token,
+      });
+    } else {
+      return res.status(400).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+
+module.exports = { userSignUp, userSignIn };
--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const { userSignUp } = require("../controller/userController");
+const { userSignUp, userSignIn } = require("../controllers/userController");
 
 userRouter.post("/api/userSignUp", userSignUp);
+userRouter.post("/api/userSignIn", userSignIn);
 
 module.exports = userRouter;
