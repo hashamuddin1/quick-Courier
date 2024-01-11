bba5486669747cf28c5adc5408e428dc3e52b738
hashamuddin1 <hashamlaptop@gmail.com>
2024-01-11 19:05:30 +0500

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT;
 require("./config/database");
 const userRouter = require("./routers/userRoute");
@@ -12,6 +13,8 @@ const issueRouter = require("./routers/issueRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use([
   userRouter,
