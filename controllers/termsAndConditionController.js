9feb20838d3a86104a368d7d1b385046d25b3021
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2024-01-23 05:11:06 +0500

    handle policies

--- controllers/termsAndConditionController.js
+++ controllers/termsAndConditionController.js
@@ -4,22 +4,24 @@ const createTermsAndCondition = async (req, res) => {
   try {
     const checkTermsAndCondition = await termsAndCondition.findOne();
     if (checkTermsAndCondition) {
-      return res.status(400).send({
-        success: false,
-        message: "Terms and Conditions are Already Created",
+      await termsAndCondition.findOneAndUpdate(
+        { _id: checkTermsAndCondition._id },
+        {
+          content: req.body.content,
+        },
+        { new: true }
+      );
+    } else {
+      const insertTermsAndCondition = new termsAndCondition({
+        content: req.body.content,
       });
+
+      await insertTermsAndCondition.save();
     }
-
-    const insertTermsAndCondition = new termsAndCondition({
-      content: req.body.content,
-    });
-
-    await insertTermsAndCondition.save();
 
     return res.status(200).send({
       success: true,
       message: "Terms And Condition has been Created Successfully",
-      data: insertTermsAndCondition,
     });
   } catch (e) {
     console.log(e);
