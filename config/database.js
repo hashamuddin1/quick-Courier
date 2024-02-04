8a217f98e2fb58dfa9250af89594e5d2c67d0bf1
hashamuddin1 <hashamlaptop@gmail.com>
2024-02-04 09:39:14 +0500

    db connection again

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://uhasham71:pgoed0veCVDh4YpA@cluster0.kz3jehe.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://uhasham71:pgoed0veCVDh4YpA@cluster0.kz3jehe.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     console.log("Database Connected Successfully");
