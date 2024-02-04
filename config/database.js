d29c4cc05c1b15bebbf9b4a92119aab65f2fcb29
hashamuddin1 <hashamlaptop@gmail.com>
2024-02-04 09:28:37 +0500

    db connection

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(`${process.env.DATABASE_CONNECTION_STRING}`)
+  .connect(
+    `mongodb+srv://uhasham71:pgoed0veCVDh4YpA@cluster0.kz3jehe.mongodb.net/?retryWrites=true&w=majority`
+  )
   .then(() => {
     console.log("Database Connected Successfully");
   })
